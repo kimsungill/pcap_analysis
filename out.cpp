66714f1c489d3caf7fdba5daf08a2cf4190e83d7
kimsungill <kdsko123@naver.com>
2019-04-14 16:32:24 +0900

    Add out modules

--- out.cpp
+++ out.cpp
@@ -1,6 +1,66 @@
 #include "out.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <netinet/ip.h>
 
-out::out()
-{
+void home (const uint8_t *data){
+    int ethlen = sizeof(ethheader);
+    struct ethheader *ethprint = (struct ethheader *)data;
+    struct ipheader *ipprint = (struct ipheader *)(data+ethlen);
+    uint8_t iphlen = ((ipprint->verlen) & 0x0F) * 4;
+    struct tcpheader *tcpprint = (struct tcpheader *)(data+ethlen+iphlen);
+    uint8_t tcphlen = ((tcpprint->offset) >> 4) * 4;
+    uint16_t httplen = ntohs(ipprint->tlan) - tcphlen - iphlen;
+    struct httpheader *httpprint = (struct httpheader *)(data+ethlen+iphlen+tcphlen);
+
+    printf("------------------------------\n");
+    printf("DMAC = ");
+    for(int i = 0; i < 6; i++){
+    printf("%02X",ethprint->mac[i]);
+    if(i < 5)
+        printf(":");
+    }
+
+    printf("\nSMAC = ");
+    for(int i = 6; i < 12; i++){
+    printf("%02X",ethprint->mac[i]);
+    if(i < 11)
+        printf(":");
+    }
+
+    if (ntohs(ethprint->type) == 0x0800 ){
+    printf("\nSIP = ");
+    for (int i = 0; i < 4; i++){
+        printf("%u",ipprint->address[i]);
+        if(i < 3)
+            printf(".");
+    }
+
+    printf("\nDIP = ");
+    for (int i = 4; i < 8; i++){
+        printf("%u",ipprint->address[i]);
+        if(i < 7)
+            printf(".");
+    }
+
+    if (ipprint->protocol == 6){
+    printf("\nSPORT = %u",ntohs(tcpprint->port[0]));
+    printf("\nDPORT = %u",ntohs(tcpprint->port[1]));
+
+    if (httplen > 16){
+    printf("\nHTTP DATA = ");
+    for(int z = 0; z < 16; z++){
+    printf("%c",httpprint->httpdata[z]);
+    }
+    } else {
+        printf("\nHTTP DATA ERROR!");
+    }
+    }
+
+    printf("------------------------------\n");
+    }
+
+    printf("\n");
 
 }
--- out.h
+++ out.h
@@ -1,11 +1,37 @@
-#ifndef OUT_H
-#define OUT_H
+#include <stdio.h>
+#include <stdint.h>
 
-
-class out
-{
-public:
-    out();
+struct ethheader {
+    uint8_t mac[12];
+    uint16_t type;
 };
 
-#endif // OUT_H
+struct ipheader {
+    uint8_t verlen;
+    uint8_t typeofse;
+    uint16_t tlan;
+    uint16_t iden;
+    uint8_t flags;
+    uint8_t offset;
+    uint8_t TTL;
+    uint8_t protocol;
+    uint16_t check;
+    uint8_t address[8];
+};
+
+struct tcpheader {
+    uint16_t port[2];
+    uint32_t sqnumber;
+    uint32_t acknumber;
+    uint8_t offset;
+    uint8_t flag;
+    uint16_t winsize;
+    uint16_t checksum;
+    uint16_t pointer;
+};
+
+struct httpheader {
+    uint8_t httpdata[16];
+};
+
+void home(const uint8_t *data);
